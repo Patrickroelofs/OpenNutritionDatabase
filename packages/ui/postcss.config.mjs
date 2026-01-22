2c6e02da566af90607ccc4100f3b2fb782a4d8c2
patrickroelofs <contact@patrickroelofs.com>
2026-01-22 18:48:16 +0100

    add tailwind-config and ui package

---
+++ packages/ui/postcss.config.mjs
@@ -0,0 +1,4 @@
+// biome-ignore lint/style/noExportedImports: allowed here as part of monorepo
+import { postcssConfig } from "@workspace/tailwind-config/postcss.config.mjs";
+
+export default postcssConfig;
