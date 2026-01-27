61f06297f0805fef436e1804177c66f2ee18500c
Patrick Roelofs <contact@patrickroelofs.com>
2026-01-27 21:16:09 +0100

    feat: add ProtectedLayout component for session management and redirection

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -1,19 +1,8 @@
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
 import DashboardHeader from "@/components/app-breadcrumbs";
 import Main from "@/components/app-main";
 import CreateNewForm from "@/forms/create-new-form";
-import { auth } from "@/lib/auth";
 
 export default async function Page() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-
-  if (!session) {
-    redirect("/");
-  }
-
   return (
     <>
       <DashboardHeader
